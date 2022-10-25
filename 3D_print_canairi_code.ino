cf42a847a1059aaf691207eb991ba6513384b908
Mads Konradsen <madskonradsen@users.noreply.github.com>
2022-10-25 14:00:52 +0200

    Formatting

--- 3D_print_canairi_code.ino
+++ 3D_print_canairi_code.ino
@@ -1,6 +1,6 @@
-#include <movingAvg.h>  
-#include <Stepper.h>
-#include <Wire.h>
+#include   
+#include 
+#include 
 #include "Adafruit_SGP30.h"
 
 // Initialize movingAvg lib
@@ -20,14 +20,13 @@ Adafruit_SGP30 sgp;
 * @param humidity [%RH]
 */
 uint32_t getAbsoluteHumidity(float temperature, float humidity) {
-    // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
-    const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
-    const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
-    return absoluteHumidityScaled;
+  // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
+  const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
+  const uint32_t absoluteHumidityScaled = static_cast(1000.0f * absoluteHumidity); // [mg/m^3]
+  return absoluteHumidityScaled;
 }
 
 void setup() {
-
   // Moving avg setup
   avgECO2.begin();
   
@@ -37,7 +36,7 @@ void setup() {
 
   Serial.println("SGP30 test");
 
-  if (!sgp.begin()){
+  if (!sgp.begin()) {
     Serial.println("Sensor not found :(");
     while (1);
   }
@@ -111,19 +110,19 @@ void loop() {
   int avg = avgECO2.reading(eCO2Val);
   //Serial.println(avg);
 
-  if (lastCheck + TEN_MINUTES < now){
+  if (lastCheck + TEN_MINUTES < now) {
     calAVG = avgECO2.getAvg();
     Serial.print("Average last 10 min = ");Serial.println(calAVG);
     
     // if the eCO2 value is above 1000 pmm && bird is up, initiate stepper to move the bird down
-    if (calAVG >= 1000 && birdUp){
+    if (calAVG >= 1000 && birdUp) {
       myStepper.step(stepsPerRevolution*2);
       delay(500);
       birdUp = !birdUp;
       Serial.print("Bird Down");
     } 
     // if the eCO2 value is below 800 pmm && bird is down, initiate stepper to move the bird up
-    if(calAVG <= 800 && !birdUp){
+    if (calAVG <= 800 && !birdUp) {
       myStepper.step(-stepsPerRevolution*2);
       birdUp = !birdUp;
       Serial.print("Bird up");
