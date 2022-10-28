9f389b3a90d3106fa1ee75e9fb2748eba67a016d
Mads Konradsen <madskonradsen@users.noreply.github.com>
2022-10-28 15:17:39 +0200

    Reverting too agressive formatting

--- 3D_print_canairi_code.ino
+++ 3D_print_canairi_code.ino
@@ -1,6 +1,6 @@
-#include   
-#include 
-#include 
+#include "movingAvg.h"  
+#include "Stepper.h"
+#include "Wire.h"
 #include "Adafruit_SGP30.h"
 
 // Initialize movingAvg lib
@@ -22,7 +22,7 @@ Adafruit_SGP30 sgp;
 uint32_t getAbsoluteHumidity(float temperature, float humidity) {
   // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
   const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
-  const uint32_t absoluteHumidityScaled = static_cast(1000.0f * absoluteHumidity); // [mg/m^3]
+  const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
   return absoluteHumidityScaled;
 }
 
